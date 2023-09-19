5334931c056dc74ad4bd6660206808ed54490eaa
ryans439 <ryans439@nycstudents.net>
2023-09-19 13:52:52 -0400

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,9 @@
-x = 3
-y = float(3)
-print(x,y)
+food = input("Enter a food: ")
+name = input("Enter a name: ")
+adjective = input("Enter an adjective: ")
+noun =  input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter a verb: ")
+verb3 = input("Enter a verb: ")
+
+print (f'It was {food} day at school, and {name} was super {adjective} for lunch. But when she went outside to eat, a {noun} stole her food! {name} chased the {noun} all over school. She {verb1}, {verb2}, and {verb3} through the play ground. Then, she tripped on her {noun}, and the {noun} escaped! Lucky, {name} and her friends were able to share their {food} with her.')
