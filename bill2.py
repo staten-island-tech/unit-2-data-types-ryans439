0b5c702391b27953b5eaf86951d36d6e2a83f587
ryans439 <ryans439@nycstudents.net>
2023-09-22 13:53:41 -0400

    Create bill2.py

---
+++ bill2.py
@@ -0,0 +1,17 @@
+g=("good")
+b=("bad")
+o=("okay")
+gr=("great")
+y=int(input("What was your bill?:"))
+x=(input("How was your service"))
+def tips(x):
+   print({x})  
+   if {g}:
+      print(1.2*y)
+   elif {b}:
+      print(y)
+   elif {o}:
+      print(1.15* y)
+   else:
+      print(1.25* y)
+tips(x)
