0b332581dfc5daf1ce9373e290795f691bb3df3b
ryans439 <ryans439@nycstudents.net>
2023-10-17 13:57:45 -0400

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -1,8 +1,8 @@
 number1 = int(input("What is Number 1?: "))
 number2 = int(input("What is Number 2?: "))
 
-def gcf(x,y):
+def gcf(x, y):
     while y:
         x, y = y, x % y
-        return x
+    return x
 print(f"The Greatest Common Factor of {number1} and {number2} is " +str(gcf(number1, number2)))
